e87a82e0928e9474c7f8e492db1d1a90c261d9f8
Christian Achatz <christian.achatz@adventure-php-framework.org>
2014-05-02 11:52:41 +0200

    Improved log output of the snapshot build script.

--- build/snapshot/create-git-snapshot.sh
+++ build/snapshot/create-git-snapshot.sh
@@ -5,6 +5,14 @@ WORKSPACE=$DIR/workspace
 REL_DIR=$DIR/../files/snapshot
 ##############################################################################
 
+echo ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+echo ":: Generate APF snapshot                                                     ::"
+echo ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+echo -n "start at: "
+date +"%Y-%m-%d, %H:%M:%S"
+echo ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+echo
+
 if [ -z "$1" ] || [ -z "$2" ]; then 
    echo "Not enough parameters. Aborting!"
    echo "Usage: $0 <git-branch> <release-version>"
@@ -16,6 +24,9 @@ fi
 GIT_BRANCH=$1
 REL_VERSION=$2
 
+echo "[INFO] Creating snapshot release for version $REL_VERSION from branch $GIT_BRANCH ..."
+echo "[INFO] Using workspace $WORKSPACE to preare snapshot release ..."
+
 # create workspace
 if [ ! -d $WORKSPACE ]; then
    mkdir -p $WORKSPACE
@@ -24,26 +35,38 @@ fi
 cd $WORKSPACE
 
 # clear workspace before export
+echo "[INFO] Clearing workspace ..."
 rm -rf *
 
 # export code from git
-git clone --depth 1 --branch $GIT_BRANCH https://github.com/AdventurePHP/code.git .
+echo "[INFO] Fetching sources from GitHub ..."
+git clone --depth 1 --branch $GIT_BRANCH https://github.com/AdventurePHP/code.git . > /dev/null 2>&1
 rm -rf .git
 
 # export config from git
+echo "[INFO] Fetching sample config from GitHub ..."
 mkdir config && cd config
-git clone --depth 1 --branch $GIT_BRANCH https://github.com/AdventurePHP/config.git .
+git clone --depth 1 --branch $GIT_BRANCH https://github.com/AdventurePHP/config.git . > /dev/null 2>&1
 rm -rf .git
 
 cd ..
 
 # create snapshot file
+echo "[INFO] Creating TGZ file ..."
 SNAPSHOT_FILE=apf-$REL_VERSION-snapshot-php5.tar.gz
 tar -czf ../$SNAPSHOT_FILE * 
 
-# more snapshot to release dir
+# remove existing prior snapshot release file
 if [ -f $REL_DIR/$SNAPSHOT_FILE ]; then
    rm -f $REL_DIR/$SNAPSHOT_FILE
 fi
 
+# transport to release dir
+echo "[INFO] Publishing snapshot file $SNAPSHOT_FILE for version $REL_VERSION from branch $GIT_BRANCH ..."
 mv ../$SNAPSHOT_FILE $REL_DIR
+
+echo
+echo ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+echo -n "end at  : "
+date +"%Y-%m-%d, %H:%M:%S"
+echo ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
