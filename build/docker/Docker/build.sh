a766b3da013ec747407259c02cb0b4d97bf9a20e
Reiner Rottmann <reiner@rottmann.it>
2018-08-19 18:09:48 +0200

    Building master by default.

--- build/docker/Docker/build.sh
+++ build/docker/Docker/build.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+branch=${1:-master}
 [ -d src ] && rm -rf src
 mkdir src && cd src
 git clone https://github.com/AdventurePHP/code.git
 cd code
-git checkout 3.4
+git checkout $branch
 cd ..
 git clone https://github.com/AdventurePHP/examples.git
 cd ..
